6bab67b258f70c86eaed0ab400ab0ed738ec6b3d
aarushgoell <aarushgoel2004@gmail.com>
2025-11-25 16:16:33 +0530

    array multiple question done

---
+++ doublenum.js
@@ -0,0 +1,11 @@
+// ✅ 1. Double each number using map
+
+// Input: [1, 2, 3, 4]
+// Output: [2, 4, 6, 8]
+// Concepts: map, arrow functions
+
+const oldArr = [1,2,3,4];
+
+const newArr = oldArr.map((el) => el * 2);
+
+console.log(newArr);
